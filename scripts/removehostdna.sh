02eaff25fa4acb98e0ec0bb5de20dd2f9d71b06d
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-03-22 15:44:30 +0100

    Update removehostdna.sh

--- scripts/removehostdna.sh
+++ scripts/removehostdna.sh
@@ -7,15 +7,15 @@ mkdir -p ${workingdirectory}/${project}/RemoveHostDNA/referencegenome
 
 #Copy host reference genome to project directory
 now=$(date +"%Y-%d-%m %H:%M:%S")
-echo "$now | Copying host genome" >>  ${workingdirectory}/${project}/run.log
+echo "$now | 		Copying host genome" >>  ${workingdirectory}/${project}/run.log
 cp ${hostgenome}* ${workingdirectory}/${project}/RemoveHostDNA/referencegenome
 genomefile=$(echo "${hostgenome}"  | sed 's/.*\///')
-echo "			Genome file: $genomefile" >> ${workingdirectory}/${project}/run.log
+echo "				Genome file: $genomefile" >> ${workingdirectory}/${project}/run.log
 
 #Index host reference genome
 if [[ $indexedhostgenome == "yes" ]]; then  
 now=$(date +"%Y-%d-%m %H:%M:%S")
-echo "$now | Indexing host genome" >> ${workingdirectory}/${project}/run.log
+echo "$now | 		Indexing host genome" >> ${workingdirectory}/${project}/run.log
 samtools faidx ${genomefile}
 bwa index ${genomefile}
 fi
@@ -23,7 +23,7 @@ fi
 #Remove host genome
 if [[ $seqtype == "SR" ]]; then 
 now=$(date +"%Y-%d-%m %H:%M:%S")
-echo "$now | Removing host DNA from SR data" >> ${workingdirectory}/${project}/run.log
+echo "$now | 		Removing host DNA from SR data" >> ${workingdirectory}/${project}/run.log
 	#Iterate through samples
 	while read samplefile; do 
 		#Select LowComplex or Original data file
@@ -33,7 +33,7 @@ echo "$now | Removing host DNA from SR data" >> ${workingdirectory}/${project}/r
 		inputfile=${workingdirectory}/${project}/RawData/${samplefile}.fastq
 		fi
 
-		echo "			Input file: $inputfile" >> ${workingdirectory}/${project}/run.log
+		echo "				Input file: $inputfile" >> ${workingdirectory}/${project}/run.log
 
 		#Map reads against the reference genome and retrieve unmapped reads
 		bwa mem -t ${threads} -R '@RG\tID:ProjectName\tCN:AuthorName\tDS:Mappingt\tPL:Illumina1.9\tSM:Sample' ${workingdirectory}/${project}/RemoveHostDNA/referencegenome/${genomefile} ${inputfile} | samtools view -b -f4 - > ${workingdirectory}/${project}/RemoveHostDNA/${samplefile}.bam
@@ -47,5 +47,3 @@ echo "Removing host DNA from PE data" >> ${workingdirectory}/${project}/run.log
 else
 echo "Sequencing read type has not been specified. It needs to be either SR or PE" >> run.log
 fi
-
-
