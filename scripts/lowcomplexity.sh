5e8677839eb6e6db6031e3734d9e976d7a4cd20e
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-03-22 15:40:55 +0100

    Update lowcomplexity.sh

--- scripts/lowcomplexity.sh
+++ scripts/lowcomplexity.sh
@@ -9,4 +9,6 @@ while read samplefile; do
 now=$(date +"%Y-%d-%m %H:%M:%S")
 echo "$now | 		Filtering sample ${samplefile}" >> ${workingdirectory}/${project}/run.log
 prinseq-lite.pl -lc_method "dust" -lc_threshold ${dustvalue} -fastq  ${workingdirectory}/${project}/RawData/${samplefile}.fastq -out_good ${workingdirectory}/${project}/LowComplexFiltered/${samplefile}.fastq -out_bad ${workingdirectory}/${project}/LowComplexFiltered/${samplefile}.lowQual
+now=$(date +"%Y-%d-%m %H:%M:%S")
+echo "$now | 		${samplefile} was successfully filtered" >> ${workingdirectory}/${project}/run.log
 done < ${metafunkdirectory}/sample.data.txt
