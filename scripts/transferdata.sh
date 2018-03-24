6524649de922fa7a5810feabba7907c428fceea3
Antton Alberdi <anttonalberdi@gmail.com>
2018-03-24 05:57:10 +0100

    s

--- scripts/transferdata.sh
+++ scripts/transferdata.sh
@@ -2,59 +2,69 @@ source "$metafunkdirectory/settings.sh"
 
 mkdir -p ${workingdirectory}/${project}/RawData
 
+#Loop across samples specified in sample.data.txt
 while read sample; do
-samplename=$(echo $sample | cut -d ' ' -f1 )
-sampleread=$(echo $sample | cut -d ' ' -f2 )
-samplefile=$(echo $sample | cut -d ' ' -f3 )
 
-#If uncompressed SR files
-if [[ $compressed == "no" && $seqtype == "SR" ]]; then
-  if [ ! -f "${datadirectory}/${samplefile}" ]; then
-    echo "File ${samplefile} was not found. Check the settings are correct."
-  fi
-  cp ${datadirectory}/${samplefile} ${workingdirectory}/${project}/RawData/
-  mv ${workingdirectory}/${project}/RawData/${samplename}.* ${workingdirectory}/${project}/RawData/${samplename}.fastq
-fi
+  #Obtain data from sample.data.txt columns
+  samplename=$(echo $sample | cut -d ' ' -f1 )
+  sampleread=$(echo $sample | cut -d ' ' -f2 )
+  samplefile=$(echo $sample | cut -d ' ' -f3 )
 
-#If uncompressed PE files
-if [[ $compressed == "no" && $seqtype == "PE" ]]; then
-  if [ ! -f "${datadirectory}/${samplefile}" ]; then
-    echo "File ${samplefile} was not found. Check the settings are correct."
+  #If uncompressed SR files
+  if [[ $compressed == "no" && $seqtype == "SR" ]]; then
+    if [ ! -f "${datadirectory}/${samplefile}" ]; then
+      echo "ERROR: File ${samplefile} was not found. Check the settings are correct."
+    else
+      echo "Transferring file ${samplefile}"
+    cp ${datadirectory}/${samplefile} ${workingdirectory}/${project}/RawData/
+    mv ${workingdirectory}/${project}/RawData/${samplename}.* ${workingdirectory}/${project}/RawData/${samplename}.fastq
+    fi
   fi
-  if [[ $sampleread == 1 ]]; then
-  cp ${datadirectory}/${samplefile} ${workingdirectory}/${project}/RawData/${samplename}_1.fastq
-  fi
-  if [[ $sampleread == 2 ]]; then
-  cp ${datadirectory}/${samplefile} ${workingdirectory}/${project}/RawData/${samplename}_2.fastq
-  fi
-fi
 
-#If compressed SR files
-if [[ $compressed == "yes" && $seqtype == "SR" ]]; then
-  if [ ! -f "${datadirectory}/${samplefile}" ]; then
-    echo "File ${samplefile} was not found. Check the settings are correct."
+  #If uncompressed PE files
+  if [[ $compressed == "no" && $seqtype == "PE" ]]; then
+    if [ ! -f "${datadirectory}/${samplefile}" ]; then
+      echo "File ${samplefile} was not found. Check the settings are correct."
+    else
+      echo "Transferring file ${samplefile}"
+      if [[ $sampleread == 1 ]]; then
+      cp ${datadirectory}/${samplefile} ${workingdirectory}/${project}/RawData/${samplename}_1.fastq
+      fi
+      if [[ $sampleread == 2 ]]; then
+      cp ${datadirectory}/${samplefile} ${workingdirectory}/${project}/RawData/${samplename}_2.fastq
+      fi
+    fi
   fi
-cp ${datadirectory}/${samplefile} ${workingdirectory}/${project}/RawData/${samplename}.fastq
-pigz -d -p ${threads} ${workingdirectory}/${project}/RawData/${samplename}.fastq
-fi
 
-#If compressed PE files
-if [[ $compressed == "yes" && $seqtype == "SR" ]]; then
-  if [ ! -f "${datadirectory}/${samplefile}" ]; then
-    echo "File ${samplefile} was not found. Check the settings are correct."
+  #If compressed SR files
+  if [[ $compressed == "yes" && $seqtype == "SR" ]]; then
+    if [ ! -f "${datadirectory}/${samplefile}" ]; then
+      echo "File ${samplefile} was not found. Check the settings are correct."
+    else
+      echo "Transferring and uncompressing file ${samplefile}"
+    cp ${datadirectory}/${samplefile} ${workingdirectory}/${project}/RawData/${samplename}.fastq
+    pigz -d -p ${threads} ${workingdirectory}/${project}/RawData/${samplename}.fastq
+    fi
   fi
-  #Process if read 1
-  if [[ $sampleread == 1 ]]; then
-  cp ${datadirectory}/${samplefile} ${workingdirectory}/${project}/RawData/${samplename}_1.fastq.gz
-  pigz -d -p ${threads} ${workingdirectory}/${project}/RawData/${samplename}_1.fastq.gz
-  fi
-  #Process if read 2
-  if [[ $sampleread == 2 ]]; then
-  cp ${datadirectory}/${samplefile} ${workingdirectory}/${project}/RawData/${samplename}_2.fastq.gz
-  pigz -d -p ${threads} ${workingdirectory}/${project}/RawData/${samplename}_2.fastq.gz
-  fi
-fi
 
+  #If compressed PE files
+  if [[ $compressed == "yes" && $seqtype == "SR" ]]; then
+    if [ ! -f "${datadirectory}/${samplefile}" ]; then
+      echo "File ${samplefile} was not found. Check the settings are correct."
+    else
+      echo "Transferring and uncompressing file ${samplefile}"
+      #Process if read 1
+      if [[ $sampleread == 1 ]]; then
+      cp ${datadirectory}/${samplefile} ${workingdirectory}/${project}/RawData/${samplename}_1.fastq.gz
+      pigz -d -p ${threads} ${workingdirectory}/${project}/RawData/${samplename}_1.fastq.gz
+      fi
+      #Process if read 2
+      if [[ $sampleread == 2 ]]; then
+      cp ${datadirectory}/${samplefile} ${workingdirectory}/${project}/RawData/${samplename}_2.fastq.gz
+      pigz -d -p ${threads} ${workingdirectory}/${project}/RawData/${samplename}_2.fastq.gz
+      fi
+    fi
+  fi
 done < ${metafunkdirectory}/sample.data.txt
 
 #Check if files were succesfully transferred
