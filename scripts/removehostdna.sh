59473d181d4398178d035b06fe6c089613f31648
Antton Alberdi <anttonalberdi@gmail.com>
2018-03-24 15:01:08 +0100

    d

--- scripts/removehostdna.sh
+++ scripts/removehostdna.sh
@@ -64,7 +64,7 @@ echo "$now | 		Removing host DNA from SR data from directory ${sourcefolder}" >>
 			samplename=$(echo $sample | cut -d ' ' -f1 )
 
 			#Map reads against the reference genome and retrieve unmapped reads
-			echo "				Removing host DNA from sample $sample" >> ${workingdirectory}/${project}/run.log
+			echo "				Removing host DNA from sample $samplename" >> ${workingdirectory}/${project}/run.log
 			bwa mem -t ${threads} -R '@RG\tID:ProjectName\tCN:AuthorName\tDS:Mappingt\tPL:Illumina1.9\tSM:Sample' ${workingdirectory}/${project}/HostDNARemoved/ReferenceGenomes/${genomefile} ${workingdirectory}/${project}/${sourcefolder}/${samplename}.fastq | samtools view -b -f4 - > ${workingdirectory}/${project}/HostDNARemoved/${samplename}.bam
 			samtools fastq -0 ${workingdirectory}/${project}/HostDNARemoved/${samplename}.fastq ${workingdirectory}/${project}/HostDNARemoved/${samplename}.bam
 			rm ${workingdirectory}/${project}/HostDNARemoved/${samplename}.bam
@@ -77,15 +77,15 @@ echo "$now | 		Removing host DNA from PE data from folder ${sourcefolder}" >> ${
 	#Loop across samples specified in sample.data.txt
 	while read sample; do
 
-		#Remove unpaired reads
-		repair.sh in=${workingdirectory}/${project}/${sourcefolder}/${samplename}_1.fastq in2=${workingdirectory}/${project}/${sourcefolder}/${samplename}_2.fastq out=${workingdirectory}/${project}/HostDNARemoved/${samplename}_1.fastq out=${workingdirectory}/${project}/HostDNARemoved/${samplename}_2.fastq
 
 		#Obtain data from sample.data.txt columns and get file name
 		samplename=$(echo $sample | cut -d ' ' -f1 )
 
 		#Map reads against the reference genome and retrieve unmapped reads
 		if [ ! -f ${workingdirectory}/${project}/HostDNARemoved/${samplename}_1.fastq ]; then
-		echo "				Removing host DNA from sample $sample" >> ${workingdirectory}/${project}/run.log
+		#Remove unpaired reads
+		repair.sh in=${workingdirectory}/${project}/${sourcefolder}/${samplename}_1.fastq in2=${workingdirectory}/${project}/${sourcefolder}/${samplename}_2.fastq out=${workingdirectory}/${project}/HostDNARemoved/${samplename}_1.fastq out=${workingdirectory}/${project}/HostDNARemoved/${samplename}_2.fastq
+		echo "				Removing host DNA from sample $samplename" >> ${workingdirectory}/${project}/run.log
 		bwa mem -t ${threads} -R '@RG\tID:ProjectName\tCN:AuthorName\tDS:Mappingt\tPL:Illumina1.9\tSM:Sample' ${workingdirectory}/${project}/HostDNARemoved/ReferenceGenomes/${genomefile} ${workingdirectory}/${project}/${sourcefolder}/${samplename}_1.fastq ${workingdirectory}/${project}/${sourcefolder}/${samplename}_2.fastq | samtools view -b -f12 - > ${workingdirectory}/${project}/HostDNARemoved/${samplename}.bam
 		samtools fastq -1 ${workingdirectory}/${project}/HostDNARemoved/${samplename}_1.fastq -2 ${workingdirectory}/${project}/HostDNARemoved/${samplename}_1.fastq ${workingdirectory}/${project}/HostDNARemoved/${samplename}.bam
 		rm ${workingdirectory}/${project}/HostDNARemoved/${samplename}.bam
