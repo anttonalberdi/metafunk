10b8f4c6cc378f7f5716043e24163a5ca2a3043d
Antton Alberdi <anttonalberdi@gmail.com>
2018-03-24 04:48:50 +0100

    w

--- scripts/transferdata.sh
+++ scripts/transferdata.sh
@@ -1,24 +1,30 @@
 source "$metafunkdirectory/settings.sh"
 
-mkdir -p ${projectdirectory}/RawData
+mkdir -p ${workingdirectory}/${project}/RawData
 
 while read samplefile; do
 
 #If uncompressed files
 if [[ $compression == "no" ]]; then
-  cp ${datadirectory}/${samplefile}.${extension} ${projectdirectory}/RawData/
-  mv ${projectdirectory}/RawData/${samplefile}.${extension} ${projectdirectory}/RawData/${samplefile}.fastq
+  cp ${datadirectory}/${samplefile}.${extension} ${workingdirectory}/${project}/RawData/
+  mv ${workingdirectory}/${project}/RawData/${samplefile}.${extension} ${workingdirectory}/${project}/RawData/${samplefile}.fastq
 fi
 
 #If compressed files
 if [[ ! $compression == "no" ]]; then
-cp ${datadirectory}/${samplefile}.${extension}.${compression} ${projectdirectory}/RawData/
-pigz -d -p ${threads} ${projectdirectory}/RawData/${samplefile}.${samplefile}.${extension}.${compression}
-mv ${projectdirectory}/RawData/${samplefile}.${extension} ${projectdirectory}/RawData/${samplefile}.fastq
+cp ${datadirectory}/${samplefile}.${extension}.${compression} ${workingdirectory}/${project}/RawData/
+pigz -d -p ${threads} ${workingdirectory}/${project}/RawData/${samplefile}.${samplefile}.${extension}.${compression}
+mv ${workingdirectory}/${project}/RawData/${samplefile}.${extension} ${workingdirectory}/${project}/RawData/${samplefile}.fastq
 fi
 
 done < ${metafunkdirectory}/sample.data.txt
 #Print stats
-filenumber=$(ls ${projectdirectory}/RawData/| wc -l)
+filenumber=$(ls ${workingdirectory}/${project}/RawData/| wc -l)
 now=$(date +"%Y-%d-%m %H:%M:%S")
-echo "$now | $filenumber files were copied and uncompressed" >> ${projectdirectory}/run.log
+echo "$now | $filenumber files were copied and uncompressed" >> ${workingdirectory}/${project}/run.log
+
+if [ -z "$(ls -A /path/to/dir)" ]; then
+   echo "Empty"
+else
+   echo "Not Empty"
+fi
