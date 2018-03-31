0411be5db69445f00973b640efb49e36576967a7
Antton Alberdi <anttonalberdi@gmail.com>
2018-03-31 09:48:42 +0200

    f

--- metafunk.sh
+++ metafunk.sh
@@ -16,6 +16,10 @@ while getopts w:d:s:f:t: option; do
   esac
 done
 
+#Get timestamp
+
+timestamp=$(date +"%Y%d%m_%H%M%S")
+
 #Get metafunk directory
 metafunkdirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -32,34 +36,34 @@ cd ${workdir}
 #########
 # Print settings and sample information to log file
 #########
-echo " " > ${workdir}/run.log
-echo "Running MetaFunk v$version pipeline with the following settings and samples:" > ${workdir}/run.log
-echo " " >> ${workdir}/run.log
-echo "##### SETTINGS ####" >> ${workdir}/run.log
-echo "Metafunk directory: $metafunkdirectory" >> ${workdir}/run.log
-echo "Working directory: $workdir" >> ${workdir}/run.log
-echo "Sample data file: $sampledatafile" >> ${workdir}/run.log
-echo "Settings file: $settingsfile" >> ${workdir}/run.log
-echo "Data directory: $datadir" >> ${workdir}/run.log
-echo "Number of threads: $threads" >> ${workdir}/run.log
-echo "Sequencing platform: $platform" >> ${workdir}/run.log
-echo " " >> ${workdir}/run.log
-echo "##### SAMPLES ####" >> ${workdir}/run.log
+echo " " > ${workdir}/run_${timestamp}.log
+echo "Running MetaFunk v$version pipeline with the following settings and samples:" > ${workdir}/run_${timestamp}.log
+echo " " >> ${workdir}/run_${timestamp}.log
+echo "##### SETTINGS ####" >> ${workdir}/run_${timestamp}.log
+echo "Metafunk directory: $metafunkdirectory" >> ${workdir}/run_${timestamp}.log
+echo "Working directory: $workdir" >> ${workdir}/run_${timestamp}.log
+echo "Sample data file: $sampledatafile" >> ${workdir}/run_${timestamp}.log
+echo "Settings file: $settingsfile" >> ${workdir}/run_${timestamp}.log
+echo "Data directory: $datadir" >> ${workdir}/run_${timestamp}.log
+echo "Number of threads: $threads" >> ${workdir}/run_${timestamp}.log
+echo "Sequencing platform: $platform" >> ${workdir}/run_${timestamp}.log
+echo " " >> ${workdir}/run_${timestamp}.log
+echo "##### SAMPLES ####" >> ${workdir}/run_${timestamp}.log
 while read sample; do
   samplename=$(echo $sample | cut -d ' ' -f1 )
   sampleinfo=$(echo $sample | cut -d ' ' -f2 )
   if [[ $sampleinfo =~ "/" && ! $sampleinfo =~ ";" ]]; then
-  echo "  $samplename PE SF" >> ${workdir}/run.log
+  echo "  $samplename PE SF" >> ${workdir}/run_${timestamp}.log
   elif [[ $sampleinfo =~ "/" && $sampleinfo =~ ";" ]]; then
-  echo "  $samplename PE MF" >> ${workdir}/run.log
+  echo "  $samplename PE MF" >> ${workdir}/run_${timestamp}.log
   elif [[ ! $sampleinfo =~ "/" && $sampleinfo =~ ";" ]]; then
-  echo "  $samplename SR MF" >> ${workdir}/run.log
+  echo "  $samplename SR MF" >> ${workdir}/run_${timestamp}.log
   else
-  echo "  $samplename SR SF" >> ${workdir}/run.log
+  echo "  $samplename SR SF" >> ${workdir}/run_${timestamp}.log
   fi
 done < ${sampledatafile}
-echo "###################" >> ${workdir}/run.log
-echo "" >> ${workdir}/run.log
+echo "###################" >> ${workdir}/run_${timestamp}.log
+echo "" >> ${workdir}/run_${timestamp}.log
 
 #########
 # Check sample.data file
@@ -71,7 +75,7 @@ cat ${sampledatafile} | awk -F ' ' -v NCOLS=3 'NF!=NCOLS{printf "Wrong number of
 # Check dependencies
 #########
 
-export workdir; export sampledatafile; export settingsfile; export datadir; export threads; export metafunkdirectory
+export workdir; export sampledatafile; export settingsfile; export datadir; export threads; export metafunkdirectory; export timestamp
 sh ${metafunkdirectory}/scripts/checkdependencies.sh
 
 #########
@@ -80,11 +84,11 @@ sh ${metafunkdirectory}/scripts/checkdependencies.sh
 now=$(date +"%Y-%d-%m %H:%M:%S")
 if [[ $copydata == "yes" ]]; then
 samplenumber=$(cat sample.data.txt | wc -l)
-echo "$now | Copying and uncompressing data files of $samplenumber samples" >> ${workdir}/run.log
-export workdir; export sampledatafile; export settingsfile; export datadir; export threads; export metafunkdirectory
+echo "$now | Copying and uncompressing data files of $samplenumber samples" >> ${workdir}/run_${timestamp}.log
+export workdir; export sampledatafile; export settingsfile; export datadir; export threads; export metafunkdirectory; export timestamp
 sh ${metafunkdirectory}/scripts/transferdata.sh
 else
-echo "$now | Data will not be copied and uncompressed" >> ${workdir}/run.log
+echo "$now | Data will not be copied and uncompressed" >> ${workdir}/run_${timestamp}.log
 fi
 
 #########
@@ -93,11 +97,11 @@ fi
 
 now=$(date +"%Y-%d-%m %H:%M:%S")
 if [[ $qualityfiltering == "yes" ]]; then
-echo "$now | Performing quality filtering" >> ${workdir}/run.log
-export workdir; export sampledatafile; export settingsfile; export datadir; export threads; export metafunkdirectory
+echo "$now | Performing quality filtering" >> ${workdir}/run_${timestamp}.log
+export workdir; export sampledatafile; export settingsfile; export datadir; export threads; export metafunkdirectory; export timestamp
 sh ${metafunkdirectory}/scripts/qualityfiltering.sh
 else
-echo "$now | Quality filtering will not be performed" >> ${workdir}/run.log
+echo "$now | Quality filtering will not be performed" >> ${workdir}/run_${timestamp}.log
 fi
 
 #########
@@ -105,11 +109,11 @@ fi
 #########
 
 if [[ $removeduplicates == "yes" ]]; then
-export workdir; export sampledatafile; export settingsfile; export datadir; export threads; export metafunkdirectory
+export workdir; export sampledatafile; export settingsfile; export datadir; export threads; export metafunkdirectory; export timestamp
 sh ${metafunkdirectory}/scripts/removeduplicates.sh
 else
 now=$(date +"%Y-%d-%m %H:%M:%S")
-echo "$now | Duplicates will not be removed" >> ${workdir}/run.log
+echo "$now | Duplicates will not be removed" >> ${workdir}/run_${timestamp}.log
 fi
 
 #########
@@ -117,11 +121,11 @@ fi
 #########
 
 if [[ $removelowcomplexity == "yes" ]]; then
-export workdir; export sampledatafile; export settingsfile; export datadir; export threads; export metafunkdirectory
+export workdir; export sampledatafile; export settingsfile; export datadir; export threads; export metafunkdirectory; export timestamp
 sh ${metafunkdirectory}/scripts/lowcomplexity.sh
 else
 now=$(date +"%Y-%d-%m %H:%M:%S")
-echo "$now | Low complexity will not be filtered" >> ${workdir}/run.log
+echo "$now | Low complexity will not be filtered" >> ${workdir}/run_${timestamp}.log
 fi
 
 #########
@@ -130,12 +134,12 @@ fi
 
 if [[ $removehostdna == "yes" ]]; then
 now=$(date +"%Y-%d-%m %H:%M:%S")
-echo "$now | Removing host DNA" >> ${workdir}/run.log
-export workdir; export sampledatafile; export settingsfile; export datadir; export threads; export metafunkdirectory
+echo "$now | Removing host DNA" >> ${workdir}/run_${timestamp}.log
+export workdir; export sampledatafile; export settingsfile; export datadir; export threads; export metafunkdirectory; export timestamp
 sh ${metafunkdirectory}/scripts/removehostdna.sh
 else
 now=$(date +"%Y-%d-%m %H:%M:%S")
-echo "$now | Host DNA will not be removed" >> ${workdir}/run.log
+echo "$now | Host DNA will not be removed" >> ${workdir}/run_${timestamp}.log
 fi
 
 #########
@@ -144,12 +148,12 @@ fi
 
 if [[ $coassembly == "yes" ]]; then
 now=$(date +"%Y-%d-%m %H:%M:%S")
-echo "$now | Co-assembling reads" >> ${workdir}/run.log
+echo "$now | Co-assembling reads" >> ${workdir}/run_${timestamp}.log
 export metafunkdirectory
 sh ${metafunkdirectory}/scripts/coassembly.sh
 else
 now=$(date +"%Y-%d-%m %H:%M:%S")
-echo "$now | Reads will not be co-assembled" >> ${workdir}/run.log
+echo "$now | Reads will not be co-assembled" >> ${workdir}/run_${timestamp}.log
 fi
 
 #########
@@ -158,12 +162,12 @@ fi
 
 if [[ $geneprediction == "yes" ]]; then
 now=$(date +"%Y-%d-%m %H:%M:%S")
-echo "$now | Predicting genes" >> ${workdir}/run.log
+echo "$now | Predicting genes" >> ${workdir}/run_${timestamp}.log
 export metafunkdirectory
 sh ${metafunkdirectory}/scripts/geneprediction.sh
 else
 now=$(date +"%Y-%d-%m %H:%M:%S")
-echo "$now | Gene prediction will not be performed" >> ${workdir}/run.log
+echo "$now | Gene prediction will not be performed" >> ${workdir}/run_${timestamp}.log
 fi
 
 #########
@@ -171,11 +175,11 @@ fi
 #########
 now=$(date +"%Y-%d-%m %H:%M:%S")
 if [[ $genemapping == "yes" ]]; then
-echo "$now | Mapping reads back to genes" >> ${workdir}/run.log
+echo "$now | Mapping reads back to genes" >> ${workdir}/run_${timestamp}.log
 export metafunkdirectory
 sh ${metafunkdirectory}/scripts/genemapping.sh
 else
-echo "$now | Gene mapping will not be performed" >> ${workdir}/run.log
+echo "$now | Gene mapping will not be performed" >> ${workdir}/run_${timestamp}.log
 fi
 
 #########
@@ -183,11 +187,11 @@ fi
 #########
 now=$(date +"%Y-%d-%m %H:%M:%S")
 if [[ $tss == "yes" || $css == "yes" ]]; then
-  echo "$now | Normalising hit and coverage tables" >> ${workdir}/run.log
+  echo "$now | Normalising hit and coverage tables" >> ${workdir}/run_${timestamp}.log
   export metafunkdirectory
   sh ${metafunkdirectory}/scripts/normalisetables.sh
   else
-  echo "$now | Hit and coverage tables will not be normalised" >> ${workdir}/run.log
+  echo "$now | Hit and coverage tables will not be normalised" >> ${workdir}/run_${timestamp}.log
 fi
 
 #########
@@ -195,24 +199,24 @@ fi
 #########
 now=$(date +"%Y-%d-%m %H:%M:%S")
 if [[ $kegg == "yes" ]]; then
-  echo "$now | Starting KEGG functional annotation" >> ${workdir}/run.log
+  echo "$now | Starting KEGG functional annotation" >> ${workdir}/run_${timestamp}.log
   export metafunkdirectory
   sh ${metafunkdirectory}/scripts/functionalannotation_kegg.sh
   else
-  echo "$now | KEGG functional annotation will not be performed" >> ${workdir}/run.log
+  echo "$now | KEGG functional annotation will not be performed" >> ${workdir}/run_${timestamp}.log
 fi
 
 now=$(date +"%Y-%d-%m %H:%M:%S")
 if [[ $eggnog == "yes" ]]; then
-  echo "$now | Starting EggNog functional annotation" >> ${workdir}/run.log
+  echo "$now | Starting EggNog functional annotation" >> ${workdir}/run_${timestamp}.log
   export metafunkdirectory
   sh ${metafunkdirectory}/scripts/functionalannotation_eggnog.sh
   else
-  echo "$now | EggNog functional annotation will not be performed" >> ${workdir}/run.log
+  echo "$now | EggNog functional annotation will not be performed" >> ${workdir}/run_${timestamp}.log
 fi
 
 #########
 # Final message
 #########
 now=$(date +"%Y-%d-%m %H:%M:%S")
-echo "$now | Congratulations, the pipeline was succesfully run" >> ${workdir}/run.log
+echo "$now | Congratulations, the pipeline was succesfully run" >> ${workdir}/run_${timestamp}.log
