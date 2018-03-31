df8a7bfd5b0f4c1df560dcf20f941f3cfdd63a1d
Antton Alberdi <anttonalberdi@gmail.com>
2018-03-31 15:06:15 +0200

    d

--- scripts/removehostdna.sh
+++ scripts/removehostdna.sh
@@ -7,35 +7,35 @@ mkdir -p ${workdir}/HostDNARemoved/ReferenceGenomes
 
 #Copy host reference genome to project directory
 now=$(date +"%Y-%d-%m %H:%M:%S")
-echo "$now | 		Copying host genome(s)" >>  ${workdir}/run.log
+echo "$now | 		Copying host genome(s)" >>  ${workdir}/run_${timestamp}.log
 
 while read sample; do
 
 	genomepath=$(echo $sample | cut -d ' ' -f3)
 	genomefile=$(echo "${genomepath}"  | sed 's/.*\///')
 	if [ ! -f ${workdir}/HostDNARemoved/ReferenceGenomes/${genomefile} ]; then
-	echo "$now |		Copying genome file $genomefile to the project directory" >> ${workdir}/run.log
+	echo "$now |		Copying genome file $genomefile to the project directory" >> ${workdir}/run_${timestamp}.log
 	cp ${genomepath}* ${workdir}/HostDNARemoved/ReferenceGenomes
 	now=$(date +"%Y-%d-%m %H:%M:%S")
-	echo "$now |		Genome file $genomefile was copied to the project directory" >> ${workdir}/run.log
+	echo "$now |		Genome file $genomefile was copied to the project directory" >> ${workdir}/run_${timestamp}.log
 	fi
 done < ${sampledatafile}
 
 #Index host reference genome
 if [[ $indexhostgenome == "yes" ]]; then
 	now=$(date +"%Y-%d-%m %H:%M:%S")
-	echo "$now | 		Indexing host genome(s)" >> ${workdir}/run.log
+	echo "$now | 		Indexing host genome(s)" >> ${workdir}/run_${timestamp}.log
 
 	while read sample; do
 		genomepath=$(echo $sample | cut -d ' ' -f3)
 		genomefile=$(echo "${genomepath}"  | sed 's/.*\///')
 		now=$(date +"%Y-%d-%m %H:%M:%S")
 		if [ ! -f ${workdir}/HostDNARemoved/ReferenceGenomes/${genomefile}.fai ]; then
-		echo "$now | 		Indexing ${genomefile} genome" >> ${workdir}/run.log
+		echo "$now | 		Indexing ${genomefile} genome" >> ${workdir}/run_${timestamp}.log
 		samtools faidx ${workdir}/HostDNARemoved/ReferenceGenomes/${genomefile}
 		bwa index ${workdir}/HostDNARemoved/ReferenceGenomes/${genomefile}
 		now=$(date +"%Y-%d-%m %H:%M:%S")
-		echo "$now | 		Genome ${genomefile} was succesfully indexed" >> ${workdir}/run.log
+		echo "$now | 		Genome ${genomefile} was succesfully indexed" >> ${workdir}/run_${timestamp}.log
 		fi
 	done < ${sampledatafile}
 
@@ -55,7 +55,7 @@ fi
 #Map to host genome
 
 now=$(date +"%Y-%d-%m %H:%M:%S")
-echo "$now | 		Removing host DNA from files in directory ${sourcefolder}" >> ${workdir}/run.log
+echo "$now | 		Removing host DNA from files in directory ${sourcefolder}" >> ${workdir}/run_${timestamp}.log
 
 while read sample; do
 
@@ -69,16 +69,16 @@ while read sample; do
 			#It is PE
 			#Remove unpaired reads
 			now=$(date +"%Y-%d-%m %H:%M:%S")
-			echo "$now | 			Repairing sample ${samplename}" >> ${workdir}/run.log
+			echo "$now | 			Repairing sample ${samplename}" >> ${workdir}/run_${timestamp}.log
 			repair.sh in=${workdir}/${sourcefolder}/${samplename}_1.fastq in2=${workdir}/${sourcefolder}/${samplename}_2.fastq out=${workdir}/HostDNARemoved/${samplename}_1.fastq out2=${workdir}/HostDNARemoved/${samplename}_2.fastq
 			#Map reads against the reference genome and retrieve unmapped reads
 			now=$(date +"%Y-%d-%m %H:%M:%S")
-			echo "$now | 			Removing host DNA from sample $samplename" >> ${workdir}/run.log
+			echo "$now | 			Removing host DNA from sample $samplename" >> ${workdir}/run_${timestamp}.log
 			bwa mem -t ${threads} -R '@RG\tID:ProjectName\tCN:AuthorName\tDS:Mappingt\tPL:Illumina1.9\tSM:Sample' ${workdir}/HostDNARemoved/ReferenceGenomes/${genomefile} ${workdir}/HostDNARemoved/${samplename}_1.fastq ${workdir}/HostDNARemoved/${samplename}_2.fastq | samtools view -b -f12 - > ${workdir}/HostDNARemoved/${samplename}.bam
 			now=$(date +"%Y-%d-%m %H:%M:%S")
 			#Check if output file has been created; otherwise, print error message and kill the job
 			if [[ ! -s ${workdir}/HostDNARemoved/${samplename}.bam ]]; then
-				echo "$now | 			ERROR: There was an error when mapping sample $samplename" >> ${workdir}/run.log
+				echo "$now | 			ERROR: There was an error when mapping sample $samplename" >> ${workdir}/run_${timestamp}.log
 				exit
 			fi
 			#Convert BAM file to FASTQ
@@ -87,21 +87,21 @@ while read sample; do
 			#Compute statistics
 	    before1=$(cat ${workdir}/${sourcefolder}/${samplename}_1.fastq | wc -l)
 	    before2=$((before1 / 4))
-	    after1=$(cat ${workdir}/DuplicatesRemoved/${samplename}_1.fastq | wc -l)
+	    after1=$(cat ${workdir}/HostDNARemoved/${samplename}_1.fastq | wc -l)
 	    after2=$((after1 / 4))
 	    difference=$((before2 - after2))
 	    percentage=$((100-(after2 * 100 / before2 )))
 			#Print statistics
-	  	echo "$now | 		From sample $samplename, $difference PE reads (${percentage}%) were mapped to the host genome" >> ${workdir}/run.log
+	  	echo "$now | 		From sample $samplename, $difference PE reads (${percentage}%) were mapped to the host genome" >> ${workdir}/run_${timestamp}.log
 
 		else
 
 			#It is SR
 			#Map reads against the reference genome and retrieve unmapped reads
-			echo "				Removing host DNA from sample $samplename" >> ${workdir}/run.log
+			echo "				Removing host DNA from sample $samplename" >> ${workdir}/run_${timestamp}.log
 			bwa mem -t ${threads} -R '@RG\tID:ProjectName\tCN:AuthorName\tDS:Mappingt\tPL:Illumina1.9\tSM:Sample' ${workdir}/HostDNARemoved/ReferenceGenomes/${genomefile} ${workdir}/${sourcefolder}/${samplename}.fastq | samtools view -b -f4 - > ${workdir}/HostDNARemoved/${samplename}.bam
 			if [[ ! -s ${workdir}/HostDNARemoved/${samplename}.bam ]]; then
-				echo "$now | 			ERROR: There was an error when mapping sample $samplename" >> ${workdir}/run.log
+				echo "$now | 			ERROR: There was an error when mapping sample $samplename" >> ${workdir}/run_${timestamp}.log
 				exit
 			fi
 			#Convert BAM file to FASTQ
@@ -110,11 +110,11 @@ while read sample; do
 			#Compute statistics
 			before1=$(cat ${workdir}/${sourcefolder}/${samplename}.fastq | wc -l)
 			before2=$((before1 / 4))
-			after1=$(cat ${workdir}/DuplicatesRemoved/${samplename}.fastq | wc -l)
+			after1=$(cat ${workdir}/HostDNARemoved/${samplename}.fastq | wc -l)
 			after2=$((after1 / 4))
 			difference=$((before2 - after2))
 			percentage=$((100-(after2 * 100 / before2 )))
 			#Print statistics
-			echo "$now | 		From sample $samplename, $difference reads (${percentage}%) were mapped to the host genome" >> ${workdir}/run.log
+			echo "$now | 		From sample $samplename, $difference reads (${percentage}%) were mapped to the host genome" >> ${workdir}/run_${timestamp}.log
 		fi
 done < ${sampledatafile}
