099287e67f8dc6febfd4d514d62406da568250dc
Antton Alberdi <anttonalberdi@gmail.com>
2018-04-04 11:22:52 +0200

    f

--- scripts/genemapping.sh
+++ scripts/genemapping.sh
@@ -1,77 +1,136 @@
-#Source dependencies
-source "$metafunkdirectory/settings.sh"
+#Source settings file
+source $settingsfile
+
 
 #Create GeneMapping directory
-mkdir -p ${workingdirectory}/${project}/GeneMapping
+mkdir -p ${workdir}/GeneMapping
 
 #Index genes
 now=$(date +"%Y-%m-%d %H:%M:%S")
-echo "$now |        Indexing gene catalogue" >> ${workingdirectory}/${project}/run.log
-samtools faidx ${workingdirectory}/${project}/GenePrediction/assembly.genes.fna
-bwa index ${workingdirectory}/${project}/GenePrediction/assembly.genes.fna
+echo "$now |        Indexing gene catalogue" >> ${workdir}/run_${timestamp}.log
+samtools faidx ${workdir}/GenePrediction/assembly.genes.fna
+bwa index ${workdir}/GenePrediction/assembly.genes.fna
 now=$(date +"%Y-%m-%d %H:%M:%S")
-echo "$now |        Gene catalogue succesfully indexed" >> ${workingdirectory}/${project}/run.log
+echo "$now |        Gene catalogue succesfully indexed" >> ${workdir}/run_${timestamp}.log
 
 #Get gene lengths
 now=$(date +"%Y-%m-%d %H:%M:%S")
-echo "$now |        Calculating gene lengths" >> ${workingdirectory}/${project}/run.log
-python ${metafunkdirectory}/scripts/contiglengths.py -i ${workingdirectory}/${project}/GenePrediction/assembly.genes.fna > ${workingdirectory}/${project}/GenePrediction/assembly.genes.lengths
-filesize=$(ls -l ${workingdirectory}/${project}/GenePrediction/assembly.genes.lengths | awk '{print $5}')
+echo "$now |        Calculating gene lengths" >> ${workdir}/run_${timestamp}.log
+python ${metafunkdirectory}/scripts/contiglengths.py -i ${workdir}/GenePrediction/assembly.genes.fna > ${workdir}/GenePrediction/assembly.genes.lengths
+filesize=$(ls -l ${workdir}/GenePrediction/assembly.genes.lengths | awk '{print $5}')
 now=$(date +"%Y-%m-%d %H:%M:%S")
 if [[ $filesize > 0 ]]; then
-echo "$now |        Gene length file was successfully created" >> ${workingdirectory}/${project}/run.log
+echo "$now |        Gene length file was successfully created" >> ${workdir}/run_${timestamp}.log
 else
-echo "$now |        There was an error while generating the gene length file" >> ${workingdirectory}/${project}/run.log
+echo "$now |        There was an error while generating the gene length file" >> ${workdir}/run_${timestamp}.log
 fi
 
-#Map reads back to genes - NOTE: THIS NEEDS TO BE UPDATED!!!!
+#Select source folder from which data will be retrieved
+if [[ "$(ls -A ${workdir}/HumanDNARemoved/)" ]]; then
+sourcefolder="HumanDNARemoved"
+repair="no"
+elif [[ "$(ls -A ${workdir}/HostDNARemoved/)" ]]; then
+sourcefolder="HostDNARemoved"
+repair="no"
+elif [[ "$(ls -A ${workdir}/LowComplexFiltered/)" ]]; then
+sourcefolder="LowComplexFiltered"
+repair="yes"
+elif [[ "$(ls -A ${workdir}/DuplicatesRemoved/)" ]]; then
+sourcefolder="DuplicatesRemoved"
+repair="yes"
+elif [[ "$(ls -A ${workdir}/QualityFiltered/)" ]]; then
+sourcefolder="QualityFiltered"
+repair="yes"
+else
+sourcefolder="RawData"
+repair="no"
+fi
 
-if [[ $seqtype == "SR" ]]; then
+
+#Map reads back to genes
 now=$(date +"%Y-%m-%d %H:%M:%S")
-echo "$now | Mapping SR reads to the gene catalogue" >> ${workingdirectory}/${project}/run.log
-	#Iterate through samples
-	while read samplefile; do
-now=$(date +"%Y-%m-%d %H:%M:%S")
-echo "$now |      Mapping sample $samplefile" >> ${workingdirectory}/${project}/run.log
-		#Select LowComplex or Original data file
-		if [[ $removelowcomplexity == "yes" ]]; then
-		inputfile=${workingdirectory}/${project}/LowComplexFiltered/${samplefile}.fastq
+echo "$now | 			Mapping reads from directory $sourcefolder to gene catalogue" >> ${workdir}/run_${timestamp}.log
+
+while read sample; do
+
+		#Obtain data from sample.data.txt columns and get file name
+		samplename=$(echo $sample | cut -d ' ' -f1)
+		sampleinfo=$(echo $sample | cut -d ' ' -f2)
+
+		if [[ $sampleinfo =~ "/" ]]; then
+			#It is PE
+			#Repair reads
+			if [[ $repair == "yes" ]]; then
+				now=$(date +"%Y-%m-%d %H:%M:%S")
+				echo "$now | 			Repairing sample ${samplename}" >> ${workdir}/run_${timestamp}.log
+				repair.sh in=${workdir}/${sourcefolder}/${samplename}_1.fastq in2=${workdir}/${sourcefolder}/${samplename}_2.fastq out=${workdir}/GeneMapping/${samplename}_1.fastq out2=${workdir}/GeneMapping/${samplename}_2.fastq
+				sourcefolder="GeneMapping"
+			fi
+			#Map reads against the gene catalogue
+			now=$(date +"%Y-%m-%d %H:%M:%S")
+			echo "$now | 			Mapping $samplename reads" >> ${workdir}/run_${timestamp}.log
+			bwa mem -t ${threads} -R '@RG\tID:ProjectName\tCN:AuthorName\tDS:Mappingt\tPL:Illumina1.9\tSM:Sample' ${workdir}/GenePrediction/assembly.genes.fna ${workdir}/${sourcefolder}/${samplename}_1.fastq ${workdir}/${sourcefolder}/${samplename}_2.fastq | samtools view -b -F0x4 - > ${workdir}/GeneMapping/${samplename}.bam
+			#Check if output file has been created; otherwise, print error message and kill the job
+			if [[ ! -s ${workdir}/GeneMapping/${samplename}.bam ]]; then
+				now=$(date +"%Y-%m-%d %H:%M:%S")
+				echo "$now | 			ERROR: There was an error when mapping sample $samplename" >> ${workdir}/run_${timestamp}.log
+				exit
+			fi
+			#Generate coverage table
+			samtools sort -T ${workdir}/GeneMapping/${samplename}.tmp.bam -o ${workdir}/GeneMapping/${samplename}.sorted.bam ${workdir}/GeneMapping/${samplename}.bam
+			samtools flagstat ${workdir}/GeneMapping/${samplename}.bam > ${workdir}/GeneMapping/${samplename}.flagstat
+				echo "$now |      Calculating coverage for $samplename" >> ${workdir}/run_${timestamp}.log
+			bedtools genomecov -ibam ${workdir}/GeneMapping/${samplename}.sorted.bam -g ${workdir}/GenePrediction/assembly.genes.lengths > ${workdir}/GeneMapping/${samplename}.cov
+			awk -F"\t" '{l[$1]=l[$1]+($2 *$3);r[$1]=$4} END {for (i in l){print i","(l[i]/r[i])}}' ${workdir}/GeneMapping/${samplename}.cov > ${workdir}/GeneMapping/${samplename}.cov.csv
+			now=$(date +"%Y-%m-%d %H:%M:%S")
+			if [[ ! -s ${workdir}/GeneMapping/${samplename}.cov.csv ]]; then
+				now=$(date +"%Y-%m-%d %H:%M:%S")
+				echo "$now | 			ERROR: There was an error when mapping sample $samplename" >> ${workdir}/run_${timestamp}.log
+				exit
+			else
+				echo "$now |      Coverage of $samplefile successfully calculated" >> ${workdir}/run_${timestamp}.log
+			fi
 		else
-		inputfile=${workingdirectory}/${project}/RawData/${samplefile}.fastq
-		fi
-		bwa mem -t ${threads} -R '@RG\tID:Project\tCN:User\tDS:Mappingt\tPL:Illumina1.9\tSM:Sample' ${workingdirectory}/${project}/GenePrediction/assembly.genes.fna ${inputfile} | samtools view -b -F0x4 - > ${workingdirectory}/${project}/GeneMapping/${samplefile}.bam
-		samtools sort -T ${workingdirectory}/${project}/GeneMapping/${samplefile}.tmp.bam -o ${workingdirectory}/${project}/GeneMapping/${samplefile}.sorted.bam ${workingdirectory}/${project}/GeneMapping/${samplefile}.bam
-		samtools flagstat ${workingdirectory}/${project}/GeneMapping/${samplefile}.bam > ${workingdirectory}/${project}/GeneMapping/${samplefile}.flagstat
-
-		now=$(date +"%Y-%m-%d %H:%M:%S")
-		echo "$now |      Sample $samplefile successfully mapped" >> ${workingdirectory}/${project}/run.log
-		echo "$now |      Calculating coverage for $samplefile" >> ${workingdirectory}/${project}/run.log
-		bedtools genomecov -ibam ${workingdirectory}/${project}/GeneMapping/${samplefile}.sorted.bam -g ${1}/9.0-GenePrediction/assembly.lengths > ${workingdirectory}/${project}/GeneMapping/${samplefile}.cov
-		awk -F"\t" '{l[$1]=l[$1]+($2 *$3);r[$1]=$4} END {for (i in l){print i","(l[i]/r[i])}}' ${workingdirectory}/${project}/GeneMapping/${samplefile}.cov > ${workingdirectory}/${project}/GeneMapping/${samplefile}.cov.csv
-		now=$(date +"%Y-%m-%d %H:%M:%S")
-		echo "$now |      Coverage of $samplefile successfully calculated" >> ${workingdirectory}/${project}/run.log
-	done < ${metafunkdirectory}/sample.data.txt
-
-elif [[ $seqtype == "PE" ]]; then
-now=$(date +"%Y-%m-%d %H:%M:%S")
-echo "$now | Removing host DNA from PE data" >> ${workingdirectory}/${project}/run.log
-
-else
-now=$(date +"%Y-%m-%d %H:%M:%S")
-echo "$now | Sequencing read type has not been specified. It needs to be either SR or PE" >> run.log
-fi
+			#It is SR
+			#Map reads against the gene catalogue
+			now=$(date +"%Y-%m-%d %H:%M:%S")
+			echo "$now | 			Mapping $samplename reads" >> ${workdir}/run_${timestamp}.log
+			bwa mem -t ${threads} -R '@RG\tID:ProjectName\tCN:AuthorName\tDS:Mappingt\tPL:Illumina1.9\tSM:Sample' ${workdir}/GenePrediction/assembly.genes.fna ${workdir}/${sourcefolder}/${samplename}.fastq | samtools view -b -F0x4 - > ${workdir}/GeneMapping/${samplename}.bam
+			#Check if output file has been created; otherwise, print error message and kill the job
+			if [[ ! -s ${workdir}/GeneMapping/${samplename}.bam ]]; then
+				now=$(date +"%Y-%m-%d %H:%M:%S")
+				echo "$now | 			ERROR: There was an error when mapping sample $samplename" >> ${workdir}/run_${timestamp}.log
+				exit
+			fi
+			#Generate coverage table
+			samtools sort -T ${workdir}/GeneMapping/${samplename}.tmp.bam -o ${workdir}/GeneMapping/${samplename}.sorted.bam ${workdir}/GeneMapping/${samplename}.bam
+			samtools flagstat ${workdir}/GeneMapping/${samplename}.bam > ${workdir}/GeneMapping/${samplename}.flagstat
+				echo "$now |      Calculating coverage for $samplename" >> ${workdir}/run_${timestamp}.log
+			bedtools genomecov -ibam ${workdir}/GeneMapping/${samplename}.sorted.bam -g ${workdir}/GenePrediction/assembly.genes.lengths > ${workdir}/GeneMapping/${samplename}.cov
+			awk -F"\t" '{l[$1]=l[$1]+($2 *$3);r[$1]=$4} END {for (i in l){print i","(l[i]/r[i])}}' ${workdir}/GeneMapping/${samplename}.cov > ${workdir}/GeneMapping/${samplename}.cov.csv
+			now=$(date +"%Y-%m-%d %H:%M:%S")
+			if [[ ! -s ${workdir}/GeneMapping/${samplename}.cov.csv ]]; then
+				now=$(date +"%Y-%m-%d %H:%M:%S")
+				echo "$now | 			ERROR: There was an error when mapping sample $samplename" >> ${workdir}/run_${timestamp}.log
+				exit
+			else
+				echo "$now |      Coverage of $samplefile successfully calculated" >> ${workdir}/run_${timestamp}.log
+			fi
+done < ${sampledatafile}
 
 #Collate coverages
-perl ${metafunkdirectory}/scripts/collatecoverages.pl ${workingdirectory}/${project}/GeneMapping/ > ${workingdirectory}/${project}/GeneCoverageTable.csv
+now=$(date +"%Y-%m-%d %H:%M:%S")
+echo "$now |      Collating coverage files" >> ${workdir}/run_${timestamp}.log
+perl ${metafunkdirectory}/scripts/collatecoverages.pl ${workdir}/GeneMapping/ > ${workdir}/GeneCoverageTable.csv
 
 #Generate hit table
-echo "$now | Generating hit table" >> ${workingdirectory}/${project}/run.log
-export WORKDIR="${workingdirectory}/${project}"
+echo "$now | Generating hit table" >> ${workdir}/run_${timestamp}.log
+export WORKDIR="${workdir}"
 Rscript ${workingdirectory}/scripts/createhittable.r --no-save
-filesize=$(ls -l ${workingdirectory}/${project}/HitTable.csv | awk '{print $5}')
+filesize=$(ls -l ${workdir}/HitTable.csv | awk '{print $5}')
 now=$(date +"%Y-%m-%d %H:%M:%S")
 if [[ $filesize > 0 ]]; then
-echo "$now |        Hit table was successfully created" >> ${workingdirectory}/${project}/run.log
+echo "$now |        Hit table was successfully created" >> ${workdir}/run_${timestamp}.log
 else
-echo "$now |        There was an error while generating the hit table" >> ${workingdirectory}/${project}/run.log
+echo "$now |        There was an error while generating the hit table" >> ${workdir}/run_${timestamp}.log
 fi
