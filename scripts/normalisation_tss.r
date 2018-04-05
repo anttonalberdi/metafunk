3a36d11b90bb62180b9809ce402f12e63c313661
Antton Alberdi <anttonalberdi@gmail.com>
2018-04-05 09:26:55 +0200

    s

--- scripts/normalisation_tss.r
+++ scripts/normalisation_tss.r
@@ -6,6 +6,7 @@ normalisationdecimals <- as.numeric(Sys.getenv("NORMALISATIONDECIMALS"))
 #Load files
 hit.table <- data.frame(fread(paste(workingdirectory,"/GeneTables/GeneHitTable.csv",sep=""),sep=",",header=TRUE),row.names=1)
 gene.lengths <- data.frame(fread(paste(workingdirectory,"/GenePrediction/assembly.genes.lengths",sep=""),sep="\t",header=FALSE),row.names=1)
+gene.lengths <- gene.lengths[rownames(hit.table),]
 totals <- colSums(hit.table)
 hit.table.tss <- round(sweep(hit.table, 2, totals, FUN="/") * normalisationscale,normalisationdecimals)
-cov.table.tss <- round(sweep(hit.table.tss, 1, gene.lengths, FUN="/"),normalisationdecimals)
+cov.table.tss <- sweep(hit.table.tss, 1, gene.lengths, FUN="/")
