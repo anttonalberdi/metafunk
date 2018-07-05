7f0d86988b6e13e2dc2b1637b375b17de653eb93
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-07-05 15:21:35 +0200

    Update normalisetables.sh

--- scripts/normalisetables.sh
+++ scripts/normalisetables.sh
@@ -1,21 +1,11 @@
 #Source settings file
 source $settingsfile
 
-#Perform TSS normalisation
-if [[ $tss == "yes" ]]; then
   now=$(date +"%Y-%m-%d %H:%M:%S")
-  echo "$now | TSS-normalising hit and coverage tables" >> ${workdir}/run_${timestamp}.log
+  echo "$now | Normalising hit and coverage tables" >> ${workdir}/run_${timestamp}.log
   export WORKDIR="${workdir}"
+  export NORMALISATIONMETHOD="${normalisationmethod}"
   export NORMALISATIONSCALE="${normalisationscale}"
   export NORMALISATIONDECIMALS="${normalisationdecimals}"
-  Rscript ${metafunkdirectory}/scripts/normalisation_tss.r --no-save
-fi
-#Perform CSS normalisation
-if [[ $css == "yes" ]]; then
-  now=$(date +"%Y-%m-%d %H:%M:%S")
-  echo "$now | CSS-normalising hit and coverage tables" >> ${workdir}/run_${timestamp}.log
-  export WORKDIR="${workdir}"
-  export NORMALISATIONSCALE="${normalisationscale}"
-  export NORMALISATIONDECIMALS="${normalisationdecimals}"
-  Rscript ${metafunkdirectory}/scripts/normalisation_css.r --no-save
-fi
+  Rscript ${metafunkdirectory}/scripts/normalisation.r --no-save
+
