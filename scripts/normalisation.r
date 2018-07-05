115468ae29c9353852e241b56f035ffee9d2f8b0
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-07-05 15:21:39 +0200

    Update and rename normalisation_tmm.r to normalisation.r

--- scripts/normalisation.r
+++ scripts/normalisation.r
@@ -2,6 +2,7 @@ library(data.table)
 library(edgeR)
 workingdirectory <- Sys.getenv("WORKDIR")
 sampledatafile <- Sys.getenv("SAMPLEDATAFILE")
+normalisationmethod <- Sys.getenv("NORMALISATIONMETHOD")
 normalisationscale <- as.numeric(Sys.getenv("NORMALISATIONSCALE"))
 normalisationdecimals <- as.numeric(Sys.getenv("NORMALISATIONDECIMALS"))
 
@@ -21,17 +22,21 @@ print("Group column does not exist.")
 dgList <- DGEList(counts=hit.table, lib.size = colSums(hit.table), genes=rownames(hit.table), group=groups, remove.zeros=TRUE)
 
 #Calculate and apply normalisation factors
+
+#Method TMM
 dgList.tmm <- calcNormFactors(dgList, method="TMM")
 tmm.nf <- dgList.tmm$samples$norm.factors
 hit.table.tmm <- round(sweep(hit.table, 2, tmm.nf, FUN="*"))
 
+#Method RLE
 dgList.RLE <- calcNormFactors(dgList, method="RLE")
 RLE.nf <- dgList.RLE$samples$norm.factors
 hit.table.RLE <- round(sweep(hit.table, 2, RLE.nf, FUN="*"))
 
+#Method UQ - PROBABLY NOT WORKING!!
 dgList.UQ <- calcNormFactors(dgList, method="upperquartile")
 UQ.nf <- dgList.UQ$samples$norm.factors
 hit.table.UQ <- round(sweep(hit.table, 2, UQ.nf, FUN="*"))
 
-
+#Method TSS
 hit.table.tss <- round(sweep(hit.table, 2, totals, FUN="/") * normalisationscale,normalisationdecimals)
