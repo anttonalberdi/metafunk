8b99e1b2bf93c9ca5c99b5b53e879d2a7540bab4
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-07-06 14:28:47 +0200

    Update normalisation.r

--- scripts/normalisation.r
+++ scripts/normalisation.r
@@ -33,7 +33,7 @@ tmm.nf <- dgList.tmm$samples$norm.factors
 hit.table.tmm <- round(sweep(hit.table, 2, tmm.nf, FUN="*"))
 hit.table.tmm <- hit.table.tmm[order(row.names(hit.table.tmm)),] 
 gene.lengths.subset <- gene.lengths[gene.lengths[,1] %in% rownames(hit.table.tmm),]
-coverage.table.tmm <- sweep(hit.table.tmm, 1, gene.lengths.subset, FUN="/")
+coverage.table.tmm <- sweep(hit.table.tmm, 1, gene.lengths.subset[,2], FUN="/")
 write.table(coverage.table.tmm,paste(workingdirectory,"/GeneTables/GeneCoverageTable.tmm.csv",sep=""),row.names=TRUE, col.names=TRUE,sep=",",quote=FALSE)
 write.table(hit.table.tmm,paste(workingdirectory,"/GeneTables/GeneHitTable.tmm.csv",sep=""),row.names=TRUE, col.names=TRUE,sep=",",quote=FALSE)
 }
@@ -45,7 +45,7 @@ rle.nf <- dgList.RLE$samples$norm.factors
 hit.table.rle <- round(sweep(hit.table, 2, rle.nf, FUN="*"))
 hit.table.rle <- hit.table.rle[order(row.names(hit.table.rle)),] 
 gene.lengths.subset <- gene.lengths[gene.lengths[,1] %in% rownames(hit.table.rle),]
-coverage.table.rle <- sweep(hit.table.rle, 1, gene.lengths.subset, FUN="/")
+coverage.table.rle <- sweep(hit.table.rle, 1, gene.lengths.subset[,2], FUN="/")
 write.table(coverage.table.rle,paste(paste(workingdirectory,"/GeneTables/GeneCoverageTable.rle.csv",sep=""),row.names=TRUE, col.names=TRUE,sep=",",quote=FALSE)
 write.table(hit.table.rle,paste(workingdirectory,"/GeneTables/GeneHitTable.rle.csv",sep=""),row.names=TRUE, col.names=TRUE,sep=",",quote=FALSE)
 }
@@ -62,7 +62,7 @@ if (grepl("tss", normalisationmethod) == TRUE){
 hit.table.tss <- round(sweep(hit.table, 2, totals, FUN="/") * normalisationscale,normalisationdecimals)
 hit.table.tss <- hit.table.tss[order(row.names(hit.table.tss)),] 
 gene.lengths.subset <- gene.lengths[gene.lengths[,1] %in% rownames(hit.table.tss),]
-coverage.table.tss <- sweep(hit.table.tss, 1, gene.lengths.subset, FUN="/")
+coverage.table.tss <- sweep(hit.table.tss, 1, gene.lengths.subset[,2], FUN="/")
 write.table(coverage.table.tss,paste(paste(workingdirectory,"/GeneTables/GeneCoverageTable.tss.csv",sep=""),row.names=TRUE, col.names=TRUE,sep=",",quote=FALSE)
 write.table(hit.table.tss,paste(workingdirectory,"/GeneTables/GeneHitTable.tss.csv",sep=""),row.names=TRUE, col.names=TRUE,sep=",",quote=FALSE)
 }
