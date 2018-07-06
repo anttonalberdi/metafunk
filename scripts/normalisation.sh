62464b96f90a69871fd0ccd28da21c1fd7c5c72d
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-07-06 07:12:29 +0200

    Update normalisation.sh

--- scripts/normalisation.sh
+++ scripts/normalisation.sh
@@ -4,6 +4,7 @@ source $settingsfile
   now=$(date +"%Y-%m-%d %H:%M:%S")
   echo "$now | Normalising hit and coverage tables using methods ${normalisationmethod}" >> ${workdir}/run_${timestamp}.log
   export WORKDIR="${workdir}"
+  export SAMPLEDATAFILE="${sampledatafile}"
   export NORMALISATIONMETHOD="${normalisationmethod}"
   export NORMALISATIONSCALE="${normalisationscale}"
   export NORMALISATIONDECIMALS="${normalisationdecimals}"
