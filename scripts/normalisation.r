d08767683e1bbe6b88495cca1c0fa083836a5922
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-07-06 10:44:18 +0200

    Update normalisation.r

--- scripts/normalisation.r
+++ scripts/normalisation.r
@@ -10,7 +10,7 @@ normalisationdecimals <- as.numeric(Sys.getenv("NORMALISATIONDECIMALS"))
 hit.table <- data.frame(fread(paste(workingdirectory,"/GeneTables/GeneHitTable.csv",sep=""),sep=",",header=TRUE),row.names=1)
 gene.lengths <- data.frame(fread(paste(workingdirectory,"/GenePrediction/assembly.genes.lengths",sep=""),sep="\t",header=FALSE))
 colnames(gene.lengths) <- c("gene","length")
-gene.lengths <- gene.lengths[order(gene),] 
+gene.lengths <- gene.lengths[order(gene.lengths[,1]),] 
 
 #Define groups
 sample.data <- read.table(sampledatafile,header=FALSE)
@@ -32,7 +32,7 @@ dgList.tmm <- calcNormFactors(dgList, method="TMM")
 tmm.nf <- dgList.tmm$samples$norm.factors
 hit.table.tmm <- round(sweep(hit.table, 2, tmm.nf, FUN="*"))
 hit.table.tmm <- hit.table.tmm[order(row.names(hit.table.tmm)),] 
-gene.lengths.subset <- gene.lengths[rownames(gene.lengths) %in% rownames(hit.table.tmm),]
+gene.lengths.subset <- gene.lengths[gene.lengths[,1] %in% rownames(hit.table.tmm),]
 coverage.table.tmm <- sweep(hit.table.tmm, 1, gene.lengths.subset, FUN="/")
 write.table(coverage.table.tmm,paste(workingdirectory,"/GeneTables/GeneCoverageTable.tmm.csv",sep=""),row.names=TRUE, col.names=TRUE,sep=",",quote=FALSE)
 write.table(hit.table.tmm,paste(workingdirectory,"/GeneTables/GeneHitTable.tmm.csv",sep=""),row.names=TRUE, col.names=TRUE,sep=",",quote=FALSE)
@@ -44,7 +44,7 @@ dgList.RLE <- calcNormFactors(dgList, method="RLE")
 rle.nf <- dgList.RLE$samples$norm.factors
 hit.table.rle <- round(sweep(hit.table, 2, rle.nf, FUN="*"))
 hit.table.rle <- hit.table.rle[order(row.names(hit.table.rle)),] 
-gene.lengths.subset <- gene.lengths[rownames(gene.lengths) %in% rownames(hit.table.rle),]
+gene.lengths.subset <- gene.lengths[gene.lengths[,1] %in% rownames(hit.table.rle),]
 coverage.table.rle <- sweep(hit.table.rle, 1, gene.lengths.subset, FUN="/")
 write.table(coverage.table.rle,paste(paste(workingdirectory,"/GeneTables/GeneCoverageTable.rle.csv",sep=""),row.names=TRUE, col.names=TRUE,sep=",",quote=FALSE)
 write.table(hit.table.rle,paste(workingdirectory,"/GeneTables/GeneHitTable.rle.csv",sep=""),row.names=TRUE, col.names=TRUE,sep=",",quote=FALSE)
@@ -61,7 +61,7 @@ hit.table.UQ <- round(sweep(hit.table, 2, UQ.nf, FUN="*"))
 if (grepl("tss", normalisationmethod) == TRUE){
 hit.table.tss <- round(sweep(hit.table, 2, totals, FUN="/") * normalisationscale,normalisationdecimals)
 hit.table.tss <- hit.table.tss[order(row.names(hit.table.tss)),] 
-gene.lengths.subset <- gene.lengths[rownames(gene.lengths) %in% rownames(hit.table.tss),]
+gene.lengths.subset <- gene.lengths[gene.lengths[,1] %in% rownames(hit.table.tss),]
 coverage.table.tss <- sweep(hit.table.tss, 1, gene.lengths.subset, FUN="/")
 write.table(coverage.table.tss,paste(paste(workingdirectory,"/GeneTables/GeneCoverageTable.tss.csv",sep=""),row.names=TRUE, col.names=TRUE,sep=",",quote=FALSE)
 write.table(hit.table.tss,paste(workingdirectory,"/GeneTables/GeneHitTable.tss.csv",sep=""),row.names=TRUE, col.names=TRUE,sep=",",quote=FALSE)
