b760c2362aebac5e268491f3c71602bf205b7c8c
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-07-08 08:26:28 +0200

    Create functionalcontrasting.sh

---
+++ scripts/functionalcontrasting.sh
@@ -0,0 +1,9 @@
+#Source settings file
+source $settingsfile
+
+now=$(date +"%Y-%m-%d %H:%M:%S")
+echo "$now | Contrasting functional profiles" >> ${workdir}/run_${timestamp}.log
+export WORKDIR="${workdir}"
+export SAMPLEDATAFILE="${sampledatafile}"
+export NORMALISATIONMETHOD="${normalisationmethod}"
+Rscript ${metafunkdirectory}/scripts/functionalcontrasting.r --no-save
