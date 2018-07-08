4ffa26535039e1c0d7b7f649dbcf5f22a2e883c9
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-07-08 08:33:10 +0200

    Update functionalcontrasting.sh

--- scripts/functionalcontrasting.sh
+++ scripts/functionalcontrasting.sh
@@ -3,8 +3,13 @@ source $settingsfile
 
 now=$(date +"%Y-%m-%d %H:%M:%S")
 echo "$now | Contrasting KEGG functional profiles" >> ${workdir}/run_${timestamp}.log
+
+#Apply annotation threshold
+awk '$5 <= ${keggthreshold} {print $0}' ${workdir}/GeneAnnotationKEGG/assembly.genes.KEGG.annotated.txt > ${workdir}/GeneAnnotationKEGG/assembly.genes.KEGG.annotated.${keggthreshold}.txt
+
+#Run analysis in R
 export WORKDIR="${workdir}"
 export SAMPLEDATAFILE="${sampledatafile}"
 export NORMALISATIONMETHOD="${normalisationmethod}"
-export ANNOTATIONTHRESHOLD="${annotationthreshold}"
+export KEGGTHRESHOLD="${keggthreshold}"
 Rscript ${metafunkdirectory}/scripts/functionalcontrasting_KEGG.r --no-save
