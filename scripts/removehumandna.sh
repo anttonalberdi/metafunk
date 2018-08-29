60153a28daa7da2e48f63fa9146ba38ef18b598c
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-08-29 08:43:22 +0200

    Update removehumandna.sh

--- scripts/removehumandna.sh
+++ scripts/removehumandna.sh
@@ -72,10 +72,16 @@ while read sample; do
 
 		if [[ $sampleinfo =~ "/" ]]; then
 			#It is PE
-			#Remove unpaired reads
+			#Decompress if needed
+			if [[ -f ${workdir}/${sourcefolder}/${samplename}_1.fastq.gz ]]; then
+			gunzip ${workdir}/${sourcefolder}/${samplename}_1.fastq.gz
+			fi
+			if [[ -f ${workdir}/${sourcefolder}/${samplename}_2.fastq.gz ]]; then
+			gunzip ${workdir}/${sourcefolder}/${samplename}_2.fastq.gz
+			fi
+			#Repair paired-end reads using BBMap script repair.sh
 			now=$(date +"%Y-%m-%d %H:%M:%S")
 			echo "$now | 			Repairing sample ${samplename}" >> ${workdir}/run_${timestamp}.log
-			#Repair paired-end reads using BBMap script repair.sh
 			repair.sh in=${workdir}/${sourcefolder}/${samplename}_1.fastq in2=${workdir}/${sourcefolder}/${samplename}_2.fastq out=${workdir}/HumanDNARemoved/${samplename}_1.fastq out2=${workdir}/HumanDNARemoved/${samplename}_2.fastq
 			#Map reads against the reference genome and retrieve unmapped reads
 			now=$(date +"%Y-%m-%d %H:%M:%S")
@@ -102,13 +108,20 @@ while read sample; do
 			now=$(date +"%Y-%m-%d %H:%M:%S")
 	  	echo "$now | 		From sample $samplename, $difference PE reads (${percentage}%) were mapped to the human genome" >> ${workdir}/run_${timestamp}.log
 	#Compress source files
-		  #now=$(date +"%Y-%m-%d %H:%M:%S")
-		  #echo "$now | 		Compressing files ${sourcefolder}/${samplename}_1.fastq and ${sourcefolder}/${samplename}_2.fastq" >> ${workdir}/run_${timestamp}.log
-		  #pigz -p ${threads} ${workdir}/${sourcefolder}/${samplename}_1.fastq
-		  #pigz -p ${threads} ${workdir}/${sourcefolder}/${samplename}_2.fastq
+		if [[ compress == TRUE ]]; then
+		  now=$(date +"%Y-%m-%d %H:%M:%S")
+		  echo "$now | 		Compressing files ${sourcefolder}/${samplename}_1.fastq and ${sourcefolder}/${samplename}_2.fastq" >> ${workdir}/run_${timestamp}.log
+		  pigz -p ${threads} ${workdir}/${sourcefolder}/${samplename}_1.fastq
+		  pigz -p ${threads} ${workdir}/${sourcefolder}/${samplename}_2.fastq
+		  fi
 		else
 
-			#It is SR
+		#It is SR
+			#Decompress if needed
+			if [[ -f ${workdir}/${sourcefolder}/${samplename}.fastq.gz ]]; then
+			gunzip ${workdir}/${sourcefolder}/${samplename}.fastq.gz
+			fi
+			
 			#Map reads against the reference genome and retrieve unmapped reads
 			now=$(date +"%Y-%m-%d %H:%M:%S")
 			echo "				Removing human DNA from sample $samplename" >> ${workdir}/run_${timestamp}.log
@@ -132,9 +145,11 @@ while read sample; do
 			#Print statistics
 			now=$(date +"%Y-%m-%d %H:%M:%S")
 			echo "$now | 		From sample $samplename, $difference reads (${percentage}%) were mapped to the human genome" >> ${workdir}/run_${timestamp}.log
-	#Compress source file
-	    #now=$(date +"%Y-%m-%d %H:%M:%S")
-	    #echo "$now | 		Compressing file ${sourcefolder}/${samplename}.fastq" >> ${workdir}/run_${timestamp}.log
-	    #pigz -p ${threads} ${workdir}/${sourcefolder}/${samplename}.fastq
+	
+	if [[ compress == TRUE ]]; then
+	    now=$(date +"%Y-%m-%d %H:%M:%S")
+	    echo "$now | 		Compressing file ${sourcefolder}/${samplename}.fastq" >> ${workdir}/run_${timestamp}.log
+	    pigz -p ${threads} ${workdir}/${sourcefolder}/${samplename}.fastq
+	fi
 fi
 done < ${sampledatafile}
