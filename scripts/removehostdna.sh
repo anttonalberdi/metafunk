f3698e9e28e432322a5fd8480d729a15724bfca8
“anttonalberdi” <anttonalberdi@gmail.com>
2019-05-02 14:06:10 +0200

    Edit to retain reads mapped to hosts

--- scripts/removehostdna.sh
+++ scripts/removehostdna.sh
@@ -4,6 +4,7 @@ source $settingsfile
 #Create LowComplexFiltered directory
 mkdir -p ${workdir}/HostDNARemoved
 mkdir -p ${workdir}/HostDNARemoved/ReferenceGenomes
+mkdir -p ${workdir}/HostDNA
 
 #Copy host reference genome to project directory and index
 now=$(date +"%Y-%m-%d %H:%M:%S")
@@ -44,7 +45,7 @@ if [ ! -f ${workdir}/HostDNARemoved/ReferenceGenomes/${genomefile} ] && [ ! -f $
 	cp ${genomepath}* ${workdir}/HostDNARemoved/ReferenceGenomes/
 	fi
 
-	
+
 	if [ ! -f ${workdir}/HostDNARemoved/ReferenceGenomes/${genomefile}.amb ] || [ ! -f ${workdir}/HostDNARemoved/ReferenceGenomes/${genomefile}.ann ] || [ ! -f ${workdir}/HostDNARemoved/ReferenceGenomes/${genomefile}.bwt ] || [ ! -f ${workdir}/HostDNARemoved/ReferenceGenomes/${genomefile}.fai ] || [ ! -f ${workdir}/HostDNARemoved/ReferenceGenomes/${genomefile}.pac ] || [ ! -f ${workdir}/HostDNARemoved/ReferenceGenomes/${genomefile}.sa ]; then
 		now=$(date +"%Y-%m-%d %H:%M:%S")
 		echo "$now | 		Indexing ${genomefile} genome" >> ${workdir}/run_${timestamp}.log
@@ -85,7 +86,7 @@ while read sample; do
 		samplename=$(echo $sample | cut -d ' ' -f1)
 		sampleinfo=$(echo $sample | cut -d ' ' -f2)
 		genomepath=$(echo $sample | cut -d ' ' -f3)
-	
+
 		if [[ $genomepath == *.fasta.gz || $genomepath == *.fa.gz ]]; then
 		genomefile=$(echo "${genomepath}"  | sed 's/.*\///' | sed 's/\.[^.]*$//')
 		else [[ $genomepath == *.fasta || $genomepath == *.fa ]]
@@ -98,12 +99,16 @@ while read sample; do
 			now=$(date +"%Y-%m-%d %H:%M:%S")
 			echo "$now | 			Repairing sample ${samplename}" >> ${workdir}/run_${timestamp}.log
 			#Repair paired-end reads using BBMap script repair.sh
-			repair.sh in=${workdir}/${sourcefolder}/${samplename}_1.fastq in2=${workdir}/${sourcefolder}/${samplename}_2.fastq out=${workdir}/HostDNARemoved/${samplename}_1.fastq out2=${workdir}/HostDNARemoved/${samplename}_2.fastq overwrite=t 
+			repair.sh in=${workdir}/${sourcefolder}/${samplename}_1.fastq in2=${workdir}/${sourcefolder}/${samplename}_2.fastq out=${workdir}/HostDNARemoved/${samplename}_1.fastq out2=${workdir}/HostDNARemoved/${samplename}_2.fastq overwrite=t
 			#Map reads against the reference genome and retrieve unmapped reads
 			now=$(date +"%Y-%m-%d %H:%M:%S")
 			echo "$now | 			Removing host DNA from sample $samplename" >> ${workdir}/run_${timestamp}.log
-			bwa mem -t ${threads} -R '@RG\tID:ProjectName\tCN:AuthorName\tDS:Mappingt\tPL:Illumina1.9\tSM:Sample' ${workdir}/HostDNARemoved/ReferenceGenomes/${genomefile} ${workdir}/HostDNARemoved/${samplename}_1.fastq ${workdir}/HostDNARemoved/${samplename}_2.fastq | samtools view | grep -v -P '^@|NM:i:[0-2]\b' | samtools view -T ${workdir}/HostDNARemoved/ReferenceGenomes/${genomefile} -b - > ${workdir}/HostDNARemoved/${samplename}.bam
-			#OLD: bwa mem -t ${threads} -R '@RG\tID:ProjectName\tCN:AuthorName\tDS:Mappingt\tPL:Illumina1.9\tSM:Sample' ${workdir}/HostDNARemoved/ReferenceGenomes/${genomefile} ${workdir}/HostDNARemoved/${samplename}_1.fastq ${workdir}/HostDNARemoved/${samplename}_2.fastq | samtools view -b -f12 - > ${workdir}/HostDNARemoved/${samplename}.bam
+			bwa mem -t ${threads} -R '@RG\tID:ProjectName\tCN:AuthorName\tDS:Mappingt\tPL:Illumina1.9\tSM:Sample' ${workdir}/HostDNARemoved/ReferenceGenomes/${genomefile} ${workdir}/HostDNARemoved/${samplename}_1.fastq ${workdir}/HostDNARemoved/${samplename}_2.fastq > ${workdir}/HostDNARemoved/${samplename}.sam
+      #Not mapped to host genome
+      samtools view ${workdir}/HostDNARemoved/${samplename}.sam | grep -v -P '^@|NM:i:[0-2]\b' | samtools view -T ${workdir}/HostDNARemoved/ReferenceGenomes/${genomefile} -b - > ${workdir}/HostDNARemoved/${samplename}.bam
+      #Mapped to host genome
+      samtools view ${workdir}/HostDNARemoved/${samplename}.sam | grep -P '^@|NM:i:[0-2]\b' | samtools view -T ${workdir}/HostDNARemoved/ReferenceGenomes/${genomefile} -b - > ${workdir}/HostDNA/${samplename}.bam
+      #OLD: bwa mem -t ${threads} -R '@RG\tID:ProjectName\tCN:AuthorName\tDS:Mappingt\tPL:Illumina1.9\tSM:Sample' ${workdir}/HostDNARemoved/ReferenceGenomes/${genomefile} ${workdir}/HostDNARemoved/${samplename}_1.fastq ${workdir}/HostDNARemoved/${samplename}_2.fastq | samtools view -b -f12 - > ${workdir}/HostDNARemoved/${samplename}.bam
 			#Check if output file has been created; otherwise, print error message and kill the job
 			if [[ ! -s ${workdir}/HostDNARemoved/${samplename}.bam ]]; then
 				now=$(date +"%Y-%m-%d %H:%M:%S")
@@ -112,7 +117,11 @@ while read sample; do
 			fi
 			#Convert BAM file to FASTQ
 			samtools fastq -s ${workdir}/HostDNARemoved/${samplename}_singleton.fastq -1 ${workdir}/HostDNARemoved/${samplename}_1.fastq -2 ${workdir}/HostDNARemoved/${samplename}_2.fastq ${workdir}/HostDNARemoved/${samplename}.bam
-			rm ${workdir}/HostDNARemoved/${samplename}.bam
+      samtools fastq -s ${workdir}/HostDNA/${samplename}_singleton.fastq -1 ${workdir}/HostDNA/${samplename}_1.fastq -2 ${workdir}/HostDNA/${samplename}_2.fastq ${workdir}/HostDNA/${samplename}.bam
+      #Remove mapping files
+      rm ${workdir}/HostDNARemoved/${samplename}.sam
+      rm ${workdir}/HostDNARemoved/${samplename}.bam
+      rm ${workdir}/HostDNA/${samplename}.bam
 			#Compute statistics
 	    before1=$(cat ${workdir}/${sourcefolder}/${samplename}_1.fastq | wc -l)
 	    before2=$((before1 / 4))
@@ -134,8 +143,12 @@ while read sample; do
 			#Map reads against the reference genome and retrieve unmapped reads
 			now=$(date +"%Y-%m-%d %H:%M:%S")
 			echo "				Removing host DNA from sample $samplename" >> ${workdir}/run_${timestamp}.log
-			bwa mem -t ${threads} -R '@RG\tID:ProjectName\tCN:AuthorName\tDS:Mappingt\tPL:Illumina1.9\tSM:Sample' ${workdir}/HostDNARemoved/ReferenceGenomes/${genomefile} ${workdir}/HostDNARemoved/${samplename}.fastq| samtools view | grep -v -P '^@|NM:i:[0-2]\b' | samtools view -T ${workdir}/HostDNARemoved/ReferenceGenomes/${genomefile} -b - > ${workdir}/HostDNARemoved/${samplename}.bam
-			#OLD: bwa mem -t ${threads} -R '@RG\tID:ProjectName\tCN:AuthorName\tDS:Mappingt\tPL:Illumina1.9\tSM:Sample' ${workdir}/HostDNARemoved/ReferenceGenomes/${genomefile} ${workdir}/${sourcefolder}/${samplename}.fastq | samtools view -b -f4 - > ${workdir}/HostDNARemoved/${samplename}.bam
+			bwa mem -t ${threads} -R '@RG\tID:ProjectName\tCN:AuthorName\tDS:Mappingt\tPL:Illumina1.9\tSM:Sample' ${workdir}/HostDNARemoved/ReferenceGenomes/${genomefile} ${workdir}/HostDNARemoved/${samplename}.fastq > ${workdir}/HostDNARemoved/${samplename}.sam
+      #Not mapped to host genome
+      samtools view ${workdir}/HostDNARemoved/${samplename}.sam  | grep -v -P '^@|NM:i:[0-2]\b' | samtools view -T ${workdir}/HostDNARemoved/ReferenceGenomes/${genomefile} -b - > ${workdir}/HostDNARemoved/${samplename}.bam
+      #Mapped to host genome
+      samtools view ${workdir}/HostDNARemoved/${samplename}.sam  | grep -P '^@|NM:i:[0-2]\b' | samtools view -T ${workdir}/HostDNA/ReferenceGenomes/${genomefile} -b - > ${workdir}/HostDNA/${samplename}.bam
+      #OLD: bwa mem -t ${threads} -R '@RG\tID:ProjectName\tCN:AuthorName\tDS:Mappingt\tPL:Illumina1.9\tSM:Sample' ${workdir}/HostDNARemoved/ReferenceGenomes/${genomefile} ${workdir}/${sourcefolder}/${samplename}.fastq | samtools view -b -f4 - > ${workdir}/HostDNARemoved/${samplename}.bam
 			if [[ ! -s ${workdir}/HostDNARemoved/${samplename}.bam ]]; then
 				now=$(date +"%Y-%m-%d %H:%M:%S")
 				echo "$now | 			ERROR: There was an error when mapping sample $samplename" >> ${workdir}/run_${timestamp}.log
@@ -143,7 +156,10 @@ while read sample; do
 			fi
 			#Convert BAM file to FASTQ
 			samtools fastq -0 ${workdir}/HostDNARemoved/${samplename}.fastq ${workdir}/HostDNARemoved/${samplename}.bam
+      #Remove mapping files
+      rm ${workdir}/HostDNARemoved/${samplename}.sam
 			rm ${workdir}/HostDNARemoved/${samplename}.bam
+      rm ${workdir}/HostDNA/${samplename}.bam
 			#Compute statistics
 			before1=$(cat ${workdir}/${sourcefolder}/${samplename}.fastq | wc -l)
 			before2=$((before1 / 4))
