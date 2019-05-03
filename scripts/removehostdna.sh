f438bcb1b5a9d0712b7951cb2d5c2499d196a62f
“anttonalberdi” <anttonalberdi@gmail.com>
2019-05-03 09:28:31 +0200

    f

--- scripts/removehostdna.sh
+++ scripts/removehostdna.sh
@@ -120,8 +120,8 @@ while read sample; do
 			samtools fastq -s ${workdir}/HostDNARemoved/${samplename}_singleton.fastq -1 ${workdir}/HostDNARemoved/${samplename}_1.fastq -2 ${workdir}/HostDNARemoved/${samplename}_2.fastq --threads ${threads} ${workdir}/HostDNARemoved/${samplename}.bam
       samtools fastq -s ${workdir}/HostDNA/${samplename}_singleton.fastq -1 ${workdir}/HostDNA/${samplename}_1.fastq -2 ${workdir}/HostDNA/${samplename}_2.fastq --threads ${threads} ${workdir}/HostDNA/${samplename}.bam
       #Remove mapping files
-      rm ${workdir}/HostDNARemoved/${samplename}_1.source.fastq
-      rm ${workdir}/HostDNARemoved/${samplename}_2.source.fastq
+      #rm ${workdir}/HostDNARemoved/${samplename}_1.source.fastq
+      #rm ${workdir}/HostDNARemoved/${samplename}_2.source.fastq
       if [[ $keep != "TRUE" ]]; then
         echo "different"
         rm ${workdir}/HostDNARemoved/${samplename}.sam
